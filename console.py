5d2be85f3bd455567d516c6db373ce77e9d28694
DikeH-Ai <56478645+DikeH-Ai@users.noreply.github.com>
2024-03-12 14:39:17 +0100

    fixed 6.1

--- console.py
+++ console.py
@@ -3,25 +3,7 @@
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    """Defines the command interpreter.
-
-    Attributes:
-        prompt (str): The command prompt.
-    """
-    prompt = "(hbnb) "
-
-    def do_quit(self, arg):
-        """Quit console"""
-        exit()
-    
-    def do_EOF(self, arg):
-        """EOF to exit console"""
-        print()
-        exit()
-    
-    def emptyline(self):
-        """Do nothing"""
-        pass
+    pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
