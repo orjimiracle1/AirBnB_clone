7d6cb18df7aa4696fd238fa8214e017beff2d9ea
DikeH-Ai <56478645+DikeH-Ai@users.noreply.github.com>
2024-03-12 01:43:33 +0100

    tester2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
+"""Module for FileStorage class."""
+import datetime
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
-
+import os
 
 class FileStorage:
     """Represent an abstracted storage engine.
